8d5c52a388cbdf74cef4ca22023250aecb8b1319
zeeshan-ad <zeeshanahmed959@gmail.com>
2023-07-18 19:12:03 +0530

    push

--- db.js
+++ db.js
@@ -8,6 +8,7 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT, // default PostgreSQL port
+  ssl: true
 });
 
 module.exports = pool;
